b12498c2f7218235553496c72d1b66f6287b2040
Aashaya Aryal <aryalaashaya@gmail.com>
2025-01-31 22:38:43 -0600

    moved frontend related files to frontend

---
+++ frontend/vite.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+  ],
+});
