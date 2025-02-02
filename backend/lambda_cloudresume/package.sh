a9ac9828831c973dc625e9560cdfe1a22acc1877
Aashaya Aryal <aryalaashaya@gmail.com>
2025-02-01 21:04:07 -0600

    debugging poetry

--- backend/lambda_cloudresume/package.sh
+++ backend/lambda_cloudresume/package.sh
@@ -4,5 +4,5 @@ set -e
 # Remove any existing zip file
 rm -f lambda.zip
 
-# Package the Lambda function code (app.py) into lambda.zip
+# Package the Lambda function code into lambda.zip
 zip -r lambda.zip app.py
